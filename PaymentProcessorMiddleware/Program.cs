cdd0858e8b2eebcf47908ba6683b0869742f6e5a
ernegreiros <negreirose48@gmail.com>
2025-07-09 12:38:55 -0300

    Added first route

--- PaymentProcessorMiddleware/Program.cs
+++ PaymentProcessorMiddleware/Program.cs
@@ -4,8 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
+var api_default = Environment.GetEnvironmentVariable("PAYMENT_PROCESSOR_URL_DEFAULT") ?? "http://payment-processor-default:8080";
+var api_fallback =Environment.GetEnvironmentVariable("PAYMENT_PROCESSOR_URL_FALLBACK") ?? "http://payment-processor-fallback:8080"; 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,10 +20,27 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-app.MapPost("/payments", async (Payment payment) =>{})
+app.MapPost("/payments", async (Transaction transaction, IHttpClientFactory httpClientFactory) =>
+{   
+    var client = httpClientFactory.CreateClient();
+    //var requestUri = new Uri($"{api_default}/payments");
+    var requestUri = new Uri($"http://localhost:8001/payments");
+
+    var requestedAt = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+    var paymentBody = new Payment(transaction.CorrelationId, transaction.Amount, requestedAt);
+
+    app.Logger.LogInformation($"Object being sent: CorrelationId: {paymentBody.CorrelationId}, Amount: {paymentBody.Amount}, RequestedAt: {requestedAt}");
+
+    using var response = await client.PostAsJsonAsync(requestUri, paymentBody);
+
+    var responseMessage = await response.Content.ReadAsStringAsync();
+
+    app.Logger.LogInformation(responseMessage);
+})
 .WithName("payments")
 .WithOpenApi();
 
 app.Run();
 
-record Payment(Guid CorrelationId, decimal Amount);
+record Transaction(Guid CorrelationId, decimal Amount);
+record Payment(Guid CorrelationId, decimal Amount, string RequestedAt);
