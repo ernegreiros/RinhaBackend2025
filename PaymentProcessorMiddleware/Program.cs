767adb66365d880fbd55a20ddbd28a305d64c27c
ernegreiros <negreirose48@gmail.com>
2025-07-08 22:28:42 -0300

    [WIP] initial configuration

---
+++ PaymentProcessorMiddleware/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapPost("/payments", async (Payment payment) =>
+{
+})
+.WithName("payments")
+.WithOpenApi();
+
+app.Run();
+
+record Payment(Guid CorrelationId, decimal Amount);
